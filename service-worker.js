84bad32ace682086abcc6a34e7d30d806a5878f0
Nazar373 <99971906+Nazar373@users.noreply.github.com>
2022-11-26 11:14:30 +0000

    Deploying to gh-pages from @ Nazar373/goit-react-hw-07-phonebook@cbbb6975f0ec6e7d1e4d0a92ecfcbfe5d1e05a68 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.16044035900d10988bf28e6d4d8424f6.js"
+  "./precache-manifest.e0813908b16eea09727742656ae1ee83.js"
 );
 
 workbox.clientsClaim();
